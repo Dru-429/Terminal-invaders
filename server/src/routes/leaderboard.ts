42f1d9a2d6213a04c91114737844223107b63b7d
Dru-429 <ect.dhruv@gmail.com>
2026-07-06 01:06:57 +0530

    Added all the leaderboard apis

--- server/src/routes/leaderboard.ts
+++ server/src/routes/leaderboard.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client/extension";
 import type { Request, Response } from "express";
-
+import { startOfWeek } from "date-fns";
+ 
 const leaderboardRoute = express.Router();
 const prisma = new PrismaClient();
 
@@ -10,7 +11,7 @@ leaderboardRoute.get("/global", async (req: Request, res: Response) => {
     const page: number = Number(req.query.page) || 1;
     const limit: number = Number(req.query.limit) || 25;
 
-    const players = await prisma.players.findMany({
+    const leadGlobal = await prisma.players.findMany({
       orderBy: {
         highestScore: "desc",
         skip: (page - 1) * limit,
@@ -19,7 +20,7 @@ leaderboardRoute.get("/global", async (req: Request, res: Response) => {
     });
 
     res.status(200).json({
-      players,
+      players: leadGlobal,
       message: "Global leaderboard feteched",
     });
   } catch (error) {
@@ -34,10 +35,77 @@ leaderboardRoute.get("/weekly", async (req: Request, res: Response) => {
   try {
     const page: number = Number(req.query.page) || 1;
     const limit: number = Number(req.query.limit) || 25;
+    const currentWeekStart = startOfWeek(new Date(), {
+      weekStartsOn: 1,
+    })
 
-  } catch (error) {}
+    const leadWeekly = await prisma.scores.groupBy({
+      by: ["playerId"], 
+      where: {
+        createdAt: {
+          gte: currentWeekStart,
+        }
+      },
+      _max: {
+        score: true,
+      },
+      orderBy: {
+        _max:{
+          score: "desc"
+        }, 
+        skip: (page - 1) * limit,
+        take: limit,
+      }
+    })
+
+    res.status(200).json({
+      pleayers: leadWeekly,
+      message: "Weekly leaderboard fetched"
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "Failed to fetch weekly leaderboard",
+    });
+  }
 });
 
-leaderboardRoute.get("/daily/?date", async (req, res) => {});
+leaderboardRoute.get("/daily", async (req: Request, res: Response) => {
+  try{
+    const page: number = Number(req.query.page) || 1;
+    const limit: number = Number(req.query.limit) || 25;
+    const currentDate = new Date();
+
+    const leadDaily = await prisma.scores.groupBy({
+      by: ["playerId"],
+      where: {
+        createdAt: {
+          gte: currentDate.setHours(0, 0, 0, 0),
+        }
+      },
+      _max: {
+        scrore: true,
+      },
+      orderBy: {
+        _max: {
+          score: "desc",
+          skip: (page -1) * limit,
+          take: limit,
+        }
+      }
+    })
+
+    res.status(200).json({
+      players: leadDaily,
+      message: "Daily leaderboard fetched"
+    })
+  }
+  catch(error){
+    console.log(error)l;
+    res.status(500).json({
+      error: "Failed to fetch"
+    })
+  }
+});
 
 export default leaderboardRoute;
