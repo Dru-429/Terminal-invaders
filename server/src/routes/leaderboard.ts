5c48b4ab53293786bf481896cb2c36b875d64976
Dru-429 <ect.dhruv@gmail.com>
2026-07-06 00:39:30 +0530

    Added global leaderboard api

--- server/src/routes/leaderboard.ts
+++ server/src/routes/leaderboard.ts
@@ -1,17 +1,43 @@
-import express from "express"
+import express from "express";
+import { PrismaClient } from "@prisma/client/extension";
+import type { Request, Response } from "express";
 
-const leaderboardRoute = express.Router()
+const leaderboardRoute = express.Router();
+const prisma = new PrismaClient();
 
-leaderboardRoute.get("/global", async (req, res) => {
+leaderboardRoute.get("/global", async (req: Request, res: Response) => {
+  try {
+    const page: number = Number(req.query.page) || 1;
+    const limit: number = Number(req.query.limit) || 25;
 
-})
+    const players = await prisma.players.findMany({
+      orderBy: {
+        highestScore: "desc",
+        skip: (page - 1) * limit,
+        take: limit,
+      },
+    });
 
-leaderboardRoute.get("/weekly", async (req, res) => {
+    res.status(200).json({
+      players,
+      message: "Global leaderboard feteched",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "Failed to fetch global leaderboard",
+    });
+  }
+});
 
-})
+leaderboardRoute.get("/weekly", async (req: Request, res: Response) => {
+  try {
+    const page: number = Number(req.query.page) || 1;
+    const limit: number = Number(req.query.limit) || 25;
 
-leaderboardRoute.get("/daily/?date", async (req, res) => {
+  } catch (error) {}
+});
 
-})
+leaderboardRoute.get("/daily/?date", async (req, res) => {});
 
-export default leaderboardRoute
+export default leaderboardRoute;
